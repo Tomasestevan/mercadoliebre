054826a403efba1a5c70d29bca7b98f40c4fd92e
Tomasestevan <tomasestevan1997@gmail.com>
2022-10-09 15:06:55 -0300

    el primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require("path")
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+
+})
+app.use(express.static('public'))
+
+app.listen(3030, ()=>  console.log('Servidor en servicio: http://localhost:3030/'));
